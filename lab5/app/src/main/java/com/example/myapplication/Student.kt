e1df53f1f86b91b34e53084778bd4d5b3d8be09c
Kacper <kpawlak2000@gmail.com>
2022-11-03 16:58:11 +0100

    lab5 finished

--- lab5/app/src/main/java/com/example/myapplication/Student.kt
+++ lab5/app/src/main/java/com/example/myapplication/Student.kt
@@ -1,7 +1,5 @@
 package com.example.myapplication
 
-//utwórz klasę Student z właściwościami: name i lastname oraz nadpisz metodę
-//toString()
 data class Student(val name: String, val lastname: String) {
     override fun toString(): String {
         return "$name $lastname"
