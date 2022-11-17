3632ee3bda74c87771d47f63fb50ad45156e140c
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2022-11-17 10:57:08 +0100

    fix

--- lab7/task2/app/src/main/java/com/example/boundservice/BindService.kt
+++ lab7/task2/app/src/main/java/com/example/boundservice/BindService.kt
@@ -2,39 +2,64 @@ package com.example.boundservice
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.Handler
+import android.os.HandlerThread
 import android.os.IBinder
-import android.os.Looper
 import android.widget.Toast
 
 class BindService : Service() {
-    var counter = 0
+    private val binder = BindServiceNew()
+    lateinit var handlerThread: HandlerThread
+    lateinit var handler: Handler
 
-    override fun onBind(p0: Intent?): IBinder? = null
+    var incrementVar: Int = 0
+    var loopWhile = false
 
-    val handler = Handler(Looper.getMainLooper())
+    inner class BindServiceNew : Binder() {
+        fun getService(): BindService = this@BindService
+    }
 
-    private val runnable = object : Runnable {
-        override fun run() {
-            counter++
-            Toast.makeText(
-                this@BindService,
-                "Your bound service is still working",
-                Toast.LENGTH_SHORT
-            ).show()
-            handler.postDelayed(this, 5000)
+    override fun onBind(intent: Intent): IBinder {
+        Toast.makeText(
+            applicationContext,
+            "Your bound service has been started",
+            Toast.LENGTH_SHORT
+        ).show()
+        return binder
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        loopWhile = false
+        handler.removeCallbacksAndMessages(null)
+        handler.looper.quit()
+        handlerThread.quit()
+        Toast.makeText(
+            applicationContext,
+            "Your bound service has been stopped",
+            Toast.LENGTH_SHORT
+        ).show()
+        return super.onUnbind(intent)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        handlerThread = HandlerThread("thread_name")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+        handler.post {
+            loopWhile = true
+            while (loopWhile) {
+                Thread.sleep(1000)
+                incrementVar++
+                if (incrementVar % 10 == 0) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Your bound service is still working",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        handler.post(runnable)
-        return START_STICKY
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        handler.removeCallbacks(runnable)
-        Toast.makeText(this, "Your bound service has been destroyed", Toast.LENGTH_SHORT).show()
-    }
-
 }
--- lab7/task2/app/src/main/java/com/example/boundservice/MainActivity.kt
+++ lab7/task2/app/src/main/java/com/example/boundservice/MainActivity.kt
@@ -1,32 +1,69 @@
 package com.example.boundservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private var countServiceBinder: BindService? = null
+    private var bounded: Boolean = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            val binder = service as BindService.BindServiceNew
+            countServiceBinder = binder.getService()
+            bounded = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            countServiceBinder = null
+            bounded = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Intent(this, BindService::class.java).also {
-            Toast.makeText(this, "Your bound service has been started", Toast.LENGTH_SHORT).show()
+        Intent(applicationContext, BindService::class.java).also { intent ->
+            bindService(
+                intent,
+                connection,
+                BIND_AUTO_CREATE
+            )
         }
 
-        val btnGoToList = findViewById<Button>(R.id.btn_go_to_list)
-        btnGoToList.setOnClickListener {
-            Intent(this, ListActivity::class.java).also {
-                startActivity(it)
-            }
+        findViewById<Button>(R.id.btn_get_counter_from_bind_service).setOnClickListener {
+            Toast.makeText(
+                applicationContext,
+                "" + (countServiceBinder?.incrementVar ?: ""),
+                Toast.LENGTH_SHORT
+            ).show()
         }
 
-        val btnGetCounter = findViewById<Button>(R.id.btn_get_counter_from_bind_service)
-        btnGetCounter.setOnClickListener {
-            Toast.makeText(this, "Counter: ${BindService().counter}", Toast.LENGTH_SHORT).show()
+        findViewById<Button>(R.id.btn_go_to_list).setOnClickListener {
+            unbindSafely()
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
         }
+    }
+
+    private fun unbindSafely() {
+        if (bounded) {
+            unbindService(connection)
+            bounded = false
+        }
+    }
+
+    override fun onStop() {
+        unbindSafely()
+        super.onStop()
     }
 
 }
